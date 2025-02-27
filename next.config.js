3622db35bf52367a3ee01c7da375d3237ef22c7b
cinder061 <hapebeast06@gmail.com>
2025-02-27 18:14:30 -0500

    Remove file-loader from webpack config

--- next.config.js
+++ next.config.js
@@ -23,19 +23,6 @@ const nextConfig = {
     ],
   },
   webpack(config, { isServer }) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    })
-
     // Ensure aliases work properly
     config.resolve.alias = {
       ...config.resolve.alias,
